aa98f9e4277511f3c945c735e072ff7c43634d16
Adam Dai <adamdai97@gmail.com>
2023-07-12 08:00:12 -0700

    working airsim2transforms for airsim to nerfstudio pipeline

--- scripts/airsim/state_logger.py
+++ scripts/airsim/state_logger.py
@@ -6,6 +6,8 @@ import numpy as np
 import airsim
 import time
 
+from terrain_nerf.airsim_utils import get_pose2D, get_pose3D
+
 client = airsim.CarClient()
 client.confirmConnection()
 
@@ -15,20 +17,27 @@ collision_count = 0
 
 try:
     while True:
-        collision_info = client.simGetCollisionInfo("Rover")  # FIXME: collisions not detected in Moon environment
-        if collision_info.has_collided:
-            collision_count += 1
-            print(f"==> Collision detected! Count = {collision_count}")
+        # collision_info = client.simGetCollisionInfo("Rover")  # FIXME: collisions not detected in Moon environment
+        # if collision_info.has_collided:
+        #     collision_count += 1
+        #     print(f"==> Collision detected! Count = {collision_count}")
+
         pose = client.simGetVehiclePose()
-        car_state = client.getCarState()
-        x = car_state.kinematics_estimated.position.x_val
-        y = car_state.kinematics_estimated.position.y_val
-        z = car_state.kinematics_estimated.position.z_val
-        speed = car_state.speed
-        yaw = airsim.utils.to_eularian_angles(pose.orientation)[2]
-        print(f"x = {x:.2f}, y = {y:.2f}, z = {z:.2f}, speed = {speed:.2f}, yaw = {yaw:.2f}")
-        state_vals = np.array([x, y, z, speed, yaw])
-        states.append(state_vals)
+        x, y, z = pose.position.x_val, pose.position.y_val, pose.position.z_val
+        pose2D = get_pose2D(client)
+        theta = np.rad2deg(pose2D[2])
+        print(f"x = {x:.2f}, y = {y:.2f}, z = {z:.2f}, theta = {theta:.2f}")
+
+
+        # car_state = client.getCarState()
+        # x = car_state.kinematics_estimated.position.x_val
+        # y = car_state.kinematics_estimated.position.y_val
+        # z = car_state.kinematics_estimated.position.z_val
+        # speed = car_state.speed
+        # yaw = airsim.utils.to_eularian_angles(pose.orientation)[2]
+        # print(f"x = {x:.2f}, y = {y:.2f}, z = {z:.2f}, speed = {speed:.2f}, yaw = {yaw:.2f}")
+        # state_vals = np.array([x, y, z, speed, yaw])
+        # states.append(state_vals)
         time.sleep(dt)
 
 except KeyboardInterrupt:
--- scripts/datagen/airsim2transforms.py
+++ scripts/datagen/airsim2transforms.py
@@ -4,74 +4,59 @@ import os
 import json
 import csv
 import imageio
+from sklearn.metrics import pairwise_distances
+from scipy.spatial.transform import Rotation as R
+
+
+#%% Helper functions
+
+def quat_to_R(quat):
+    """Quaternion in scalar-last (x, y, z, w) format"""
+    r = R.from_quat(quat)
+    return r.as_matrix()
+
+
+def euler_to_R(euler, seq='XYZ'):
+    r = R.from_euler(seq, euler, degrees=True)
+    return r.as_matrix()
+
+
+def quat_to_euler(quat):
+    """Quaternion in scalar-last (x, y, z, w) format"""
+    r = R.from_quat(quat)
+    return r.as_euler('xyz', degrees=True)
+
+
+def axis_angle_to_rot_mat(axis, angle):
+    """Angle in radians"""
+    axis = axis / np.linalg.norm(axis)
+    q = np.hstack((axis * np.sin(angle/2), np.cos(angle/2)))
+    return quat_to_R(q)
 
 
 def get_intrinsic(imgdir):
     imgfiles = [os.path.join(imgdir, f) for f in sorted(os.listdir(imgdir)) if f.endswith('JPG') or f.endswith('jpg') or f.endswith('png') or f.endswith('jpeg')]
 
     H, W, C = imageio.imread(imgfiles[0]).shape
-    vfov = 90
+    vfov = 40
 
     focal_y = H / 2  / np.tan(np.deg2rad(vfov/2))
     focal_x = H / 2  / np.tan(np.deg2rad(vfov/2))
 
     return H, W, focal_x, focal_y
     
-def eulerAnglesToRotationMatrix(theta) :
-    R_x = np.array([[1,         0,                  0                   ],
-                    [0,         math.cos(theta[0]), -math.sin(theta[0]) ],
-                    [0,         math.sin(theta[0]), math.cos(theta[0])  ]
-                    ])
-    R_y = np.array([[math.cos(theta[1]),    0,      math.sin(theta[1])  ],
-                    [0,                     1,      0                   ],
-                    [-math.sin(theta[1]),   0,      math.cos(theta[1])  ]
-                    ])
-    R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),    0],
-                    [math.sin(theta[2]),    math.cos(theta[2]),     0],
-                    [0,                     0,                      1]
-                    ])
-    R = np.dot(R_z, np.dot( R_y, R_x ))
-    return R
-
-
-def pad_rot(rot):
-    padh = lambda x: np.hstack([x, np.zeros((x.shape[0], 1))])
-    padv = lambda x: np.vstack([x, np.zeros((1, x.shape[1]))])
-
-    rot_mat = padv(padh(rot))
-    rot_mat[-1,-1] = 1
-    return rot_mat
-
-
-def quaternion_to_euler(q_w, q_x, q_y, q_z):
-    # Convert quaternion to rotation matrix
-    rotation_matrix = np.array([
-        [1 - 2*q_y*q_y - 2*q_z*q_z, 2*q_x*q_y - 2*q_w*q_z, 2*q_x*q_z + 2*q_w*q_y],
-        [2*q_x*q_y + 2*q_w*q_z, 1 - 2*q_x*q_x - 2*q_z*q_z, 2*q_y*q_z - 2*q_w*q_x],
-        [2*q_x*q_z - 2*q_w*q_y, 2*q_y*q_z + 2*q_w*q_x, 1 - 2*q_x*q_x - 2*q_y*q_y]
-    ])
-
-    # Extract Euler angles from rotation matrix
-    roll = np.arctan2(rotation_matrix[2, 1], rotation_matrix[2, 2])
-    pitch = np.arctan2(-rotation_matrix[2, 0], np.sqrt(rotation_matrix[2, 1]**2 + rotation_matrix[2, 2]**2))
-    yaw = np.arctan2(rotation_matrix[1, 0], rotation_matrix[0, 0])
-
-    # Convert angles to degrees
-    roll = np.degrees(roll)
-    pitch = np.degrees(pitch)
-    yaw = np.degrees(yaw)
-
-    return roll, pitch, yaw
 
 def config_parser():
     import configargparse
     parser = configargparse.ArgumentParser()
     parser.add_argument("datadir", type=str, help='path to your meta')
-    parser.add_argument("filename", type=str, help='file name')
+    parser.add_argument("--filename", type=str, default='airsim_rec.txt', help='file name')
     parser.add_argument("--imgdir", type=str, default='images', help='image directory name')
     
     return parser
     
+
+#%% Main
 
 if __name__ == '__main__':
 
@@ -79,6 +64,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
  
     data = {}
+    ds_rate = 2
 
     with open(os.path.join(args.datadir, args.filename), 'r') as file:
         reader = csv.reader(file, delimiter='\t')
@@ -86,100 +72,70 @@ if __name__ == '__main__':
 
         data['cameraFrames'] = []
 
-        for row in reader:
-            vehicle_name = row[0]
-            timestamp = int(row[1])
-            pos_x = float(row[2])
-            pos_y = float(row[3])
-            pos_z = float(row[4])
-            q_w = float(row[5])
-            q_x = float(row[6])
-            q_y = float(row[7])
-            q_z = float(row[8])
-            image_file = row[9]
+        for i, row in enumerate(reader):
+            if i % ds_rate == 0:
+                vehicle_name = row[0]
+                timestamp = int(row[1])
+                pos_x = float(row[2])
+                pos_y = float(row[3])
+                pos_z = float(row[4])
+                q_w = float(row[5])
+                q_x = float(row[6])
+                q_y = float(row[7])
+                q_z = float(row[8])
+                image_file = row[9]
 
-            roll, pitch, yaw = quaternion_to_euler(q_w, q_x, q_y, q_z)
-            
-            data['cameraFrames'].append({
-                'position': {
-                    'x': pos_x,
-                    'y': pos_y,
-                    'z': pos_z
-                },
-                'rotation': {
-                    'x': roll,
-                    'y': pitch,
-                    'z': yaw
-                },
-                'image': image_file
-                ,
-                'timestamp': timestamp
-            })
-            
-
-
-
-    GES_pos = np.array([[data['cameraFrames'][i]['position']['x'], 
-                            data['cameraFrames'][i]['position']['y'],
-                            data['cameraFrames'][i]['position']['z']] 
-                        for i in range(len(data['cameraFrames']))])
+                roll, pitch, yaw = quat_to_euler([q_x, q_y, q_z, q_w])
+                
+                data['cameraFrames'].append({
+                    'position': {
+                        'x': pos_x,
+                        'y': pos_y,
+                        'z': pos_z
+                    },
+                    'rotation': {
+                        'x': roll,
+                        'y': pitch,
+                        'z': yaw,
+                        'qvec': [q_x, q_y, q_z, q_w]
+                    },
+                    'image': image_file
+                    ,
+                    'timestamp': timestamp
+                })
 
     H, W, focal_x, focal_y = get_intrinsic(os.path.join(args.datadir, args.imgdir))
 
-    # rescale the whole range if you want
-    scale = 2**3 * np.pi / max(GES_pos.max(), -GES_pos.min())
-    SS = np.eye(4)
-    SS[0,0] = scale
-    SS[1,1] = scale
-    SS[2,2] = scale
-    
-    #rot_ECEF2ENUV = np.array([[0, 0, -1], [1, 0, 0], [0, -1, 0]])  # airsim camera frame transformation
-    rot_ECEF2ENUV = np.array([[0, 0, 1], 
-                              [1, 0, 0], 
-                              [0, 1, 0]]) 
-
-    nxyz = []
     frames = []
-    transforms = []
-    
     image_list = np.sort(os.listdir(os.path.join(args.datadir, args.imgdir)))
-
     print("Found images...", len(image_list))
-    
     print("Found entries...", len(data['cameraFrames']))
+
     for i in range(len(data['cameraFrames'])):
         position = data['cameraFrames'][i]['position']
-        pos_x = position['x'] / 50
-        pos_y = position['y'] / 50
-        pos_z = position['z'] / 50
-        xyz = np.array([pos_x, pos_y, pos_z])
-        # [pos_e,pos_n,pos_u] = np.dot(rot_ECEF2ENUV, xyz)
-        #[pos_e,pos_n,pos_u] = xyz
-        # NED to ENU
-        pos_e = xyz[1]
-        pos_n = xyz[0]
-        pos_u = -xyz[2]
-        print(pos_e,pos_n,pos_u)
+        pos_x = position['x']
+        pos_y = position['y']
+        pos_z = position['z']
+        xyz = np.array([pos_x, -pos_y, -pos_z])
 
-        rotation = data['cameraFrames'][i]['rotation']
+        roll = data['cameraFrames'][i]['rotation']['x']
+        pitch = data['cameraFrames'][i]['rotation']['y']
+        yaw = data['cameraFrames'][i]['rotation']['z']
+        print("Roll", roll, "Pitch", pitch, "Yaw", yaw)
+        
+        # Swap pitch and roll
+        rotation = euler_to_R([pitch, roll, yaw], seq='xyz')
+        translation = xyz.reshape(3, 1)
+        
+        c2w = np.concatenate([rotation, translation], 1)
+        c2w = np.concatenate([c2w, np.array([[0, 0, 0, 1]])], 0)
 
-        x = np.radians(rotation['x'])
-        y = np.radians(rotation['y'])
-        z = np.radians(rotation['z'])
-        # print(x, y, z)
+        # Align camera
+        x_axis = c2w[:3, 0]
+        init_R = axis_angle_to_rot_mat(x_axis, np.deg2rad(90))  # Local 90 X
+        init_R = euler_to_R([-90], seq='z') @ init_R  # Global 90 Z
 
-        rot_mat = np.linalg.inv(eulerAnglesToRotationMatrix([x, y, z]))
-        rot_mat = np.dot(rot_ECEF2ENUV, rot_mat)
-        GES_rotmat = pad_rot(rot_mat)
-
-        xyz  = np.array([pos_e,pos_n,pos_u,1])[None,:]
-        nx,ny,nz = np.dot(SS, xyz.T)[:3,0]
-        # print(nx,ny,nz)
-        nxyz.append([nx,ny,nz])
-        GES_rotmat[:3,3] = np.array([nx,ny,nz])
-
-        c2w = np.concatenate([GES_rotmat[:3,:4], np.array([[0, 0, 0, 1]])], 0)
-        transforms.append(c2w)
+        c2w[:3,:3] = init_R @ c2w[:3,:3]
         
         if not os.path.exists(os.path.join(args.datadir, args.imgdir, data['cameraFrames'][i]['image'])):
             print("Image not found", os.path.join(args.imgdir, data['cameraFrames'][i]['image']))
@@ -194,8 +150,6 @@ if __name__ == '__main__':
         frames.append(frame)
 
     print("Frames: ", len(frames))
-    nxyz = np.array(nxyz)
-    dists = np.sqrt(np.sum(nxyz**2, -1))
 
     out = {
         "w": W,
@@ -210,15 +164,9 @@ if __name__ == '__main__':
     out["k2"] = 0.0
     out["p1"] = 0.0
     out["p2"] = 0.0
-    out["scale"] = scale
         
     out["frames"] = frames
-    # applied_transform = np.eye(4)[:3, :]
-    # out["applied_transform"] = applied_transform.tolist()
 
     print("Saving...", os.path.join(args.datadir, 'transforms.json'))
     with open(os.path.join(args.datadir, 'transforms.json'), 'w', encoding="utf-8") as f: 
         json.dump(out, f, indent=4)
-    
-    # save transforms
-    #np.save(os.path.join(args.datadir, 'transforms.npy'), np.array(transforms))
