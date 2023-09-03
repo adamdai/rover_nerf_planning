e443f5f0b1dfe11f137d23171d79fd3c601ed309
Adam Dai <adamdai97@gmail.com>
2023-09-03 15:17:24 -0700

    nerf_features.py ray rendering working

--- scripts/nerf_features.py
+++ scripts/nerf_features.py
@@ -6,6 +6,7 @@ Run from /nerstudio_ws directory
 """
 
 import torch
+import matplotlib.pyplot as plt
 from pathlib import Path
 from nerfstudio.utils.eval_utils import eval_setup
 from nerfstudio.cameras.rays import RayBundle
@@ -15,12 +16,13 @@ torch.manual_seed(42)
 
 #%% ------------------------------------ Parameters ------------------------------------ %%
 
+# Nerfstudio poster example
+#config, pipeline, checkpoint_path, _ = eval_setup(Path('outputs/poster/nerfacto/2023-09-03_160741/config.yml'))
+
 # Moon Test Scenario
 config, pipeline, checkpoint_path, _ = eval_setup(Path('outputs/MoonTestScenario/nerfacto/2023-07-21_013712/config.yml'))
-save_folder = "MoonTestScenario/features/"
 
 print(f"Using checkpoint_path: {checkpoint_path}")
-print(f"Will save to {save_folder}")
 
 
 #%% ------------------------------------ Functions ------------------------------------ %%
@@ -54,24 +56,43 @@ def nerf_render_rays(origins, directions):
 if __name__ == "__main__":
 
     # Grid of xy points in NeRF coordinates ([-1, 1] x [-1, 1])
-    x = torch.linspace(-1, 1, 10)
-    y = torch.linspace(-1, 1, 10)
+    z = 1.0
+
+    bounds = 1.0
+    N_res = 100
+
+    x = torch.linspace(-bounds, bounds, N_res)
+    y = torch.linspace(-bounds, bounds, N_res)
+
     xx, yy = torch.meshgrid(x, y, indexing='ij')
-    xyz = torch.stack([xx.flatten(), yy.flatten(), torch.zeros_like(xx.flatten())], dim=-1)
+    #xyz = torch.stack([xx.flatten(), yy.flatten(), torch.zeros_like(xx.flatten())], dim=-1)
+    xyz = torch.stack([xx.flatten(), yy.flatten(), z*torch.ones_like(xx.flatten())], dim=-1)
+    
     origins = torch.tensor(xyz, device=pipeline.device)
+    #origins = xyz.clone().detach().to(pipeline.device)
 
     N_rays = len(origins)
 
-    # origins = torch.zeros((N_rays, 3), device=pipeline.device)
-
     # All rays pointing down
-    directions = torch.tensor([[0, 0, 1]], device=pipeline.device).repeat(N_rays, 1)
+    directions = torch.tensor([[0.0, 0.0, -1.0]], device=pipeline.device).repeat(N_rays, 1)
 
     print("Generating features for NeRF coordinates")
     rgbd = nerf_render_rays(origins, directions)
 
-    print(rgbd['rgb'])
-    print(rgbd['accumulation'])
-    print(rgbd['depth'])
+    # print(rgbd['rgb'])
+    # print(rgbd['accumulation'])
+    # print(rgbd['depth'])
+    rgb = rgbd['rgb'].detach().cpu().numpy()
+    print(rgb.shape)
+    rgb = rgb.reshape((N_res, N_res, 3))
+    print(rgb.shape)
+
+    fig = plt.figure()
+    plt.imshow(rgb)
+    #plt.show()
+    # save figure
+    fig.savefig('nerf_features.png')
+
+    print("saved image")
 
 
